4aa62841fec79441b6222119808367ac6edc438e
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-07-31 18:42:01 +0000

    agregando un for

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const dominios= document.getElementById('dominios');
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net']
 
 let domain = '';
 for(let i=0; i<pronoun.length; i++){
     for(let j=0; j<adj.length; j++){
         for(let k=0; k<noun.length; k++){
-            domain = pronoun[i]+adj[j]+noun[k]+'.com'
-
-            dominios.innerHTML+=`<p>${domain}</p>`
-            console.log(domain)
+            for(let m=0; m<extensions.length; m++){
+                domain = pronoun[i]+adj[j]+noun[k]+extensions[m]
+                
+                dominios.innerHTML+=`<p>${domain}</p>`
+                console.log(domain)
+            }
         }
     }
 }
