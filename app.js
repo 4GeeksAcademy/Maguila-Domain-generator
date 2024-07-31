0195489fed1cb6a736d98d518f9cdedb185d704f
MaguiSanchez <162079957+MaguiSanchez@users.noreply.github.com>
2024-07-31 17:29:12 +0000

    My domain generator

---
+++ app.js
@@ -0,0 +1,17 @@
+const dominios= document.getElementById('dominios');
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+let domain = '';
+for(let i=0; i<pronoun.length; i++){
+    for(let j=0; j<adj.length; j++){
+        for(let k=0; k<noun.length; k++){
+            domain = pronoun[i]+adj[j]+noun[k]+'.com'
+
+            dominios.innerHTML+=`<p>${domain}</p>`
+            console.log(domain)
+        }
+    }
+}
